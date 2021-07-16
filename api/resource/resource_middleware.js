1641a0bde9398aa929b36041f55969632c005b19
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-16 11:14:29 -0600

    update to router and middleware resouce

--- api/resource/resource_middleware.js
+++ api/resource/resource_middleware.js
@@ -1,7 +1,12 @@
-const Resource = require('./model')
+// const Resource = require('./model')
 
+const checkResourcePayload = (req, res, next) => {
+    if(!req.body.resource_name) return next({
+        status: 400, message: 'resource name missing'
+    })
 
+}
 
 module.exports = {
-
+    checkResourcePayload
 }
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,29 @@
 // build your `/api/resources` router here
+const express = require('express');
+
+const Resource = require('./model');
+const { checkResourcePayload } = require('./resource_middleware.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try{
+        const resource = await Resource.getAll()
+        res.json(resource)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', checkResourcePayload, async (req, res, next) => {
+    try{
+        const newResource = await Resource.create(req.body)
+        res.json(newResource)
+    }
+    catch (err){
+        next(err)
+    }
+})
+
+module.exports = router;
