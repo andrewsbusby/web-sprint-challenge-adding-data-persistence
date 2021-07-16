381e5bc4992e45b7e20d503ecda93b62d2589de0
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-16 11:33:22 -0600

    update to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router')
+const projectRouter = require('./project/router')
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
+server.use('/api/project', projectRouter);
 
 module.exports = server;
